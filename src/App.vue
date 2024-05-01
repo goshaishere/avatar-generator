f7d9e64711c749d32df44e8827952b98df828cf4
Егор Еремин <goshaishere@list.ru>
2024-05-01 08:48:53 +0300

    add navlinks

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,14 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
 <template>
   <RouterView />
+
+  <header>
+    <nav class="navigation-links">
+      <RouterLink to="/">генератор</RouterLink>
+      <RouterLink to="/favorites">избранное</RouterLink>
+    </nav>
+  </header>
 </template>
